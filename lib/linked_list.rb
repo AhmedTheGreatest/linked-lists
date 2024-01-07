2aad7233fe81dff4807cdddd027666c337739cdd
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-01-07 10:58:10 +0500

    Add tail method to linked list class to find the tail node

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,5 +2,21 @@
 
 require './lib/node'
 
+# A Class representing a Linked List
 class LinkedList
+  def initialize
+    @head = Node.new
+  end
+
+  def tail(node = @head)
+    return node if last_node?(node)
+
+    tail(node.next_node)
+  end
+
+  def last_node?(node)
+    return true if node.next_node.nil?
+
+    false
+  end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
 require './lib/linked_list'
+
+list = LinkedList.new
+puts list
+puts list.tail.next_node
